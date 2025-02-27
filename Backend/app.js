84e056bbf4a843b22bd258f7caaa885559858efa
prakash suthar <prakashgajjar096@gmail.com>
2025-02-27 12:15:10 +0530

    Added frontend files

--- Backend/app.js
+++ Backend/app.js
@@ -16,6 +16,7 @@ app.use(express.urlencoded({extended:true}));
 
 app.use('/api',SignUpRoutes);
 
+
 const PORT = process.env.PORT ||5000;
 app.listen(PORT , ()=>{
     console.log('Server is running on port ',PORT);
